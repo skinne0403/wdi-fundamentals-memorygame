5bfc2a52617419856fa2842dfcac5be7bf8d0183
Kinne <Samantha.L.Kinne@disney.com>
2018-01-14 18:26:06 -0800

    created game logic

--- Memory Game/js/main.js
+++ Memory Game/js/main.js
@@ -1,6 +1,13 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardInPlay = [];
+var cardOne = cards[0];
+cardInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+var cardTwo = cards[2];
+cardInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if (cardInPlay[0] === cardInPlay[1]) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
