bcbd7555e3aab40541ec279525214fe46c205f24
Kinne <Samantha.L.Kinne@disney.com>
2018-01-18 17:44:57 -0800

    created game logic

--- Memory Game/js/main.js
+++ Memory Game/js/main.js
@@ -1,13 +1,36 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardInPlay = [];
-var cardOne = cards[0];
-cardInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if (cardInPlay[0] === cardInPlay[1]) {
-  alert("You found a match!");
+
+var checkForMatch = function(){
+  if (cardInPlay[0] === cardInPlay[1]) {
+console.log("You found a match!");
 } else {
-  alert("Sorry, try again.");
+console.log("Sorry, try again.");
 }
+};
+
+var flipCard = function(cardId){
+  if (cardInPlay[0] === cardInPlay[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+    console.log("User flipped "+ cards[cardId]);
+    checkForMatch();
+};
+flipCard(0);
+flipCard(2);
+
+// question... should there be a push?
+//Should it be alert or console.log
+ //call the checkForMatch function from inside the flipCard function, where the lines above used to sit.
+//Use the push method to add
+//the card that the user flipped (cards[cardId])
+//
+// previous var cardOne = cards[0];
+//cardsInPlay.push(cardOne);
+//console.log("User flipped " + cardOne);
+
+//var cardTwo = cards[2];
+//cardsInPlay.push(cardTwo);
+//console.log("User flipped " + cardTwo);
