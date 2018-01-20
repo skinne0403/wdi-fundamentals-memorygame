6a5d5f09e51230f9f10255ac0c4d598625ec0d68
Kinne <Samantha.L.Kinne@disney.com>
2018-01-20 12:31:03 -0800

    created game logic

--- Memory Game/js/main.js
+++ Memory Game/js/main.js
@@ -1,7 +1,29 @@
-var cards = ["queen", "queen", "king", "king"];
+
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 var cardInPlay = [];
 
-var checkForMatch = function(){
+var checkForMatch = function() {
   if (cardInPlay[0] === cardInPlay[1]) {
 console.log("You found a match!");
 } else {
@@ -9,20 +31,22 @@ console.log("Sorry, try again.");
 }
 };
 
-var flipCard = function(cardId){
+var flipCard = function(cardId) {
   if (cardInPlay[0] === cardInPlay[1]) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
   }
-    console.log("User flipped "+ cards[cardId]);
+    console.log("User flipped "+ cards[cardId].rank);
+    cardInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
     checkForMatch();
 };
 flipCard(0);
 flipCard(2);
 
-// question... should there be a push?
-//Should it be alert or console.log
+// deleted: var cards = ["queen", "queen", "king", "king"];
  //call the checkForMatch function from inside the flipCard function, where the lines above used to sit.
 //Use the push method to add
 //the card that the user flipped (cards[cardId])
